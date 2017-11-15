2155cd3b2380b379596dc047f5d043bce36a4195
kgonzalez452 <kgonzalez@techlaunch.io>
2017-11-14 20:18:29 -0500

    first commit

---
+++ quiz.js
@@ -0,0 +1,52 @@
+    var items = [
+        {
+            itemid: 111111,
+            name: "Krabby Patty",
+            price: 8.00,
+            description: "A burger made with a secret formula from the Krusty Krab."
+        },
+        {
+            itemid: 111123,
+            name: "Pizza Rolls",
+            price: 5.00,
+            description: "a bowl of pizza in a few bite sized pieces."
+        },
+        {
+            itemid: 111234,
+            name: "Chicken Quesadilla",
+            price: 8.50,
+            description: "A delicious cheesy toasted chicken quesadilla"
+        },
+        {
+            itemid: 112345,
+            name: "Capri Sun Juice Pouch",
+            price: 0.50,
+            description: "Juice that you coulde drink in seconds while wanting to have more."
+        },
+        {
+            itemid: 123456,
+            name: "PB & J Sandwich",
+            price: 2.50,
+            description: "If you want to have a throwback to your childhood while you eat, have this and itll bring you back memories from your childhood."
+        },
+    ]
+
+    class NewProducts{
+        constructor(arrayitems){
+            this.arrayitems = arrayitems
+        }
+        getNames(array){
+            var newArray = arrayitems.map(function(items){
+                return arrayitems.sort(itemid)
+            })
+          }
+          getSaleOrderItems(){
+              
+        }
+        orderItems(){
+            
+        }
+    };
+    // console.log(NewProducts(newArray))
+    console.log(NewProducts);
+    // console.log()
